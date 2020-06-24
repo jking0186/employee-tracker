2e2f5f514d717246ee49ea3ec29bc531a654ff8f
Joe King <jking0186@gmail.com>
2020-06-24 09:39:37 -0500

    running DB

--- server.js
+++ server.js
@@ -3,14 +3,32 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "",
     database: "employee_DB"
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "View departments",
+                "View roles",
+                "View employees",
+                "Update employee roles",
+                "exit"
+            ]
+        })
+    }
