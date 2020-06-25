330bcf050983a8c4c8b3b7ef23f43bc54bddc007
Joe King <jking0186@gmail.com>
2020-06-24 20:23:47 -0500

    added prompts

--- server.js
+++ server.js
@@ -31,4 +31,80 @@ function start() {
                 "exit"
             ]
         })
-    }
+        .then(function (answer) {
+            switch (answer.action) {
+                case "Add a department":
+                    addDepartment();
+                    break;
+
+                case "Add a role":
+                    addRole();
+                    break;
+
+                case "Add an employee":
+                    addEmployee();
+                    break;
+
+                case "View departments":
+                    viewDepartment();
+                    break;
+
+                case "View roles":
+                    viewRole();
+                    break;
+
+                case "View employees":
+                    viewEmployee();
+                    break;
+
+                case "Update employee roles":
+                    viewEmployee();
+                    break;
+            }
+        })
+}
+
+function addDepartment() {
+    inquirer
+        .prompt({
+            name: "department",
+            type: "input",
+            message: "What department would you like to add?"
+        })
+        .then()
+}
+
+function addRole() {
+    inquirer
+        .prompt({
+            name: "title",
+            type: "input",
+            message: "What role would you like to add?"
+        })
+        .then()
+}
+
+function addEmployee() {
+    inquirer
+    .prompt({
+        name: "employeeFirst",
+        type: "input",
+        message: "What is the empoyee's first name?"
+    },
+    {
+        name: "employeeLast",
+        type: "input",
+        message: "What is the employee's last name?"
+    })
+}
+
+
+//   .then(function(answer) {
+    //     var query = "SELECT position, song, year FROM top5000 WHERE ?";
+    //     connection.query(query, { artist: answer.artist }, function(err, res) {
+    //       for (var i = 0; i < res.length; i++) {
+    //         console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+    //       }
+    //       start();
+    //     });
+    //   });
